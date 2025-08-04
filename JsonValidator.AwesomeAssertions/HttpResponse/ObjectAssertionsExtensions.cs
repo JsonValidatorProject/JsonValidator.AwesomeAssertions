c200a52c1157fff8210eaea72d2db9a051c7a240
Balázs Kis <github.easel273@passmail.net>
2025-08-04 09:27:59 +0200

    initial commit

---
+++ JsonValidator.AwesomeAssertions/HttpResponse/ObjectAssertionsExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using AwesomeAssertions;
+using AwesomeAssertions.Execution;
+using AwesomeAssertions.Primitives;
+
+namespace JsonValidator.AwesomeAssertions.HttpResponse;
+
+public static class ObjectAssertionsExtensions
+{
+    /// <summary>
+    /// Checks whether the HTTP response's body as a JSON string matches the input object in both structure and values.
+    /// </summary>
+    /// <param name="instance">The <see cref="ObjectAssertions"/></param>
+    /// <param name="expected">The expected value as an anonymous object</param>
+    /// <param name="because">The reason the two values should match</param>
+    /// <param name="becauseArgs">The parameters for the reason argument</param>
+    public static AndConstraint<ObjectAssertions> HaveJsonBody(
+        this ObjectAssertions instance,
+        object expected,
+        string because = "",
+        params object[] becauseArgs)
+    {
+        if (instance.Subject is not HttpResponseMessage responseMessage)
+        {
+            throw new InvalidOperationException("This method should only be used on HttpResponseMessage objects.");
+        }
+
+        var isMatch = System.Text.Json.JsonDocument
+            .Parse(responseMessage.Content.ReadAsStringAsync().GetAwaiter().GetResult())
+            .TryValidateMatch(expected, out var errors);
+
+        AssertionChain
+            .GetOrCreate()
+            .BecauseOf(because, becauseArgs)
+            .ForCondition(isMatch)
+            .FailWith("Expected response body to match the JSON input, but found differences {0}.", errors);
+
+        return new AndConstraint<ObjectAssertions>(instance);
+    }
+}
---
+++ JsonValidator.AwesomeAssertions/Json/JsonDocumentAssertions.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using AwesomeAssertions;
+using AwesomeAssertions.Execution;
+
+namespace JsonValidator.AwesomeAssertions.Json;
+
+public class JsonDocumentAssertions
+{
+    private readonly JsonDocument _instance;
+
+    public JsonDocumentAssertions(JsonDocument instance) => _instance = instance;
+
+    /// <summary>
+    /// Checks whether the JSON document matches the input object in both structure and values.
+    /// </summary>
+    /// <param name="expected">The expected value as an anonymous object</param>
+    /// <param name="because">The reason the two values should match</param>
+    /// <param name="becauseArgs">The parameters for the reason argument</param>
+    public AndConstraint<JsonDocumentAssertions> Match(
+        object expected,
+        string because = "",
+        params object[] becauseArgs)
+    {
+        var isMatch = _instance.TryValidateMatch(expected, out var errors);
+
+        AssertionChain
+            .GetOrCreate()
+            .BecauseOf(because, becauseArgs)
+            .ForCondition(isMatch)
+            .FailWith("Expected object to match the JSON input, but found differences {0}.", errors);
+
+        return new AndConstraint<JsonDocumentAssertions>(this);
+    }
+}
